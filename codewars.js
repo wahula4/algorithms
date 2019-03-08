366c9a158ee3b19038a335f554cd620590b93d5a
Tony Wahula <mawahula4@gmail.com>
2019-03-07 17:07:08 -0700

    initial commit

---
+++ codewars.js
@@ -0,0 +1,8 @@
+(fizzbuzz = () => {
+  for (i = 1; i < 11; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+    else if (i % 3 === 0) console.log("fizz");
+    else if (i % 5 === 0) console.log("buzz");
+    else console.log(i);
+  }
+})();
