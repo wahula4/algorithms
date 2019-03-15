808b5117a205e3ee202254d8da03bcbd68170053
Tony Wahula <mawahula4@gmail.com>
2019-03-14 18:35:49 -0600

    map and filter

--- codewars.js
+++ codewars.js
@@ -7,17 +7,26 @@
 //   }
 // })();
 
-(checkPrime = (num) => {
-    if(num < 1)
-    return console.log(false)
-    if(num === 2 || num === 1)
-    return console.log(true)
-    
-    let divisor = 2;
-    while(num > divisor){
-        if(num % divisor === 0) 
-        return console.log(false);
-        else divisor++;
-    }
-    console.log(true);
-})(3)
+// (checkPrime = (num) => {
+//     if(num < 1)
+//     return console.log(false)
+//     if(num === 2 || num === 1)
+//     return console.log(true)
+
+//     let divisor = 2;
+//     while(num > divisor){
+//         if(num % divisor === 0) 
+//         return console.log(false);
+//         else divisor++;
+//     }
+//     console.log(true);
+// })(7)
+
+// get all unique values
+const array = [1,2,2,3,4,5,6,6]
+const array2= ["Todd", "billy", "Billy", "Fred"]
+let uniques = [...new Set(array)]
+console.log(uniques)
+let lowered = array2.map(word => word.toLowerCase())
+let unique = lowered.filter((value, index, arr) => arr.indexOf(value) === index)
+console.log(unique)
